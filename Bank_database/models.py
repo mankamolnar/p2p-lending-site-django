42a10c415c20762dd7cfb87ff1d78691200bf7ae
Kelltaronn <volenta94@gmail.com>
2023-07-29 18:11:50 +0200

    New model made named as Szamla

--- Bank_database/models.py
+++ Bank_database/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Szamla(models.Model):
+    aktualis_osszeg = models.FloatField()
+    szamla_tulajdonos = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
+    szamla_tipus = models.TextField(max_length=255)
