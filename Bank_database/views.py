726095f86e2f335869f415af511b144fb303d862
mankamolnar <markmanomolnar@gmail.com>
2023-08-05 09:36:38 -0600

    Registration modul had been added

--- Bank_database/views.py
+++ Bank_database/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+        login(request, user)
+    return render(request, 'registration/registration.html', {'form': form})
