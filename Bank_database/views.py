c4c1ddc5810a586dd9ddde63b7d7aaaf624603c8
Kelltaronn <138993725+Kelltaronn@users.noreply.github.com>
2023-09-30 17:02:01 +0200

    Bank_Database_changes

--- Bank_database/views.py
+++ Bank_database/views.py
@@ -1,34 +1,63 @@
-from django.shortcuts import render ,redirect
+#Imports:
+
+from django.shortcuts import render,redirect,HttpResponse
+
+#Django implemented model imports:
 from django.contrib.auth.models import User,auth
-from django.contrib import messages
+
+#Django implemented form imports:
 from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
+
+
+#Django error imports:
+from django.contrib import messages
+
+#Function imports
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from Bank_database.form import Szamlaform , AddToBalanceForm
+
+#Form imports:
+from Bank_database.form import Szamlaform , AddToBalanceForm , Userform ,CustomUserCreationForm
+
+#Model imports:
 from Bank_database.models import Szamla
-# from Bank_database.form import Loginform
+
+#Error code imports:
+from django.core.exceptions import ObjectDoesNotExist
+
+#404 Error handling import:
+from django.shortcuts import get_object_or_404
+
+#Login imports:
+from django import forms
+from django.forms.widgets import TextInput
+
 
 
 # Create your views here.
 def main(request):
     if request.user.is_authenticated:
         user = request.user
-        print(user)
-        szamla = Szamla.objects.get(szamla_tulajdonos=user.id)# Model/Helyi
+        szamla = get_object_or_404(Szamla,szamla_tulajdonos=user.id)# Model/Helyi
         context = {"user":user,"szamla":szamla}
         return render(request,"logged_in_main_webpage.html",context)
+
+
     else:
         return render(request, "index.html", {})
 
-
+#Login View ()
 def logged(request):
+    print("Ez az a view")
     if request.method == "GET":
         context = {
-            "userform": AuthenticationForm()
+            "form": Userform(),
         }
         return render(request,"registration/login.html",context)
-    if request.method == "POST":
-        print("correct")
+    
+
+#Custom login:
+'''    if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
 
@@ -38,11 +67,12 @@ def logged(request):
             return redirect('')
         else:
             messages.info(request, "Invalid Username or Password")
-        return redirect('login')
+        return redirect('login')'''
 
-# Create your views here.
+#Register function:
 def register(request):
-    form = UserCreationForm(request.POST or None)
+    #form = UserCreationForm(request.POST or None)
+    form = CustomUserCreationForm(request.POST or None)
     if form.is_valid():
         user_datas = form.save()
         szamla = Szamla(aktualis_osszeg=0,szamla_tulajdonos=user_datas,szamla_tipus=request.POST['account_type'])
@@ -54,7 +84,7 @@ def register(request):
         return render(request, 'registration/registration.html', {'form': form})
     
 
-#Functions
+#Functions which used for the pages:
 
 def add_currency_to_account(request):
     if request.method == "GET":
@@ -68,5 +98,9 @@ def add_currency_to_account(request):
         account_data.save()
         context = {}
         return redirect("main_page")
+    
+#404 Error handling:
+def error_404(request,exception):
+    return render(request,"error404.html",{})
 
 
